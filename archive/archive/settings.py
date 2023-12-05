8f5d39072079c2deadf24ddfc7e6a2f9578c97dc
gkozhemiakin <kga.2004@mail.ru>
2023-12-05 18:52:24 +0300

    Перешел с SQLite на PostgreSQL

--- archive/archive/settings.py
+++ archive/archive/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'archive.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Database_PostgreSQL_Archive',
+        'USER': 'postgres',
+        'PASSWORD': '12345!',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
